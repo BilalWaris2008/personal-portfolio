d21fc1b25a900d1fff417e652b7afbdd29bb0e99
M.Bilal Waris <bilalwaris543@gmail.com>
2026-01-14 23:46:03 +0500

    Added Functionality on clicking Resume Lists

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const resumeLists = document.querySelectorAll('.resume-list');
 
-
-
 // resume section when clicking tab lists
 resumeLists.forEach((list,idx) => {
     list.addEventListener('click', () =>{
+        document.querySelector('.resume-list.active').classList.remove('active');
         list.classList.add('active');
     })
 });
