1c13a383b9cf471fbb6bcf5d4b45253eafb8d31e
M.Bilal Waris <bilalwaris543@gmail.com>
2026-01-14 23:38:31 +0500

    Added Active Functionality for Resume Section

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const resumeLists = document.querySelectorAll('.resume-list');
+
+
+
+// resume section when clicking tab lists
+resumeLists.forEach((list,idx) => {
+    list.addEventListener('click', () =>{
+        list.classList.add('active');
+    })
+});
